c8de4cdd5acbde107d1425ebf0190bcf5d4c1b6e
prakhar17072004 <pmankur2004@gmail.com>
2024-08-17 18:38:53 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+;
+let mySize = 6;
+console.log(mySize);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+
+// let age:number =34;
+// if(age<50)
+//  age+=10;
+
+// console.log(age);
+
+// let user:[number,string]=[1,'prakhar'];
+// user.push(1);
+// const enum Size {Small=5,Medium,Large};
+// let mySize =Size.Medium;
+// console.log(mySize);
+//funtion define
+ function sum(a:number,b:number):number{
+    return a+b;
+ }
+
+ console.log(sum(2,5));
