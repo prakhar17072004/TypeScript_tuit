c39419f3f9299d6cfaa330214d1257c7c64533a8
prakhar17072004 <pmankur2004@gmail.com>
2024-08-17 20:06:24 +0530

    new file is added

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,32 @@
 // let mySize =Size.Medium;
 // console.log(mySize);
 //funtion define
- function sum(a:number,b:number):number{
-    return a+b;
- }
+let a:number=23;
+let b:string="hello";
+let c:boolean=false;
+let f:string="hello";
+let k:string="hello";
+let g:string="hello";
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(f);
+console.log(g);
 
- console.log(sum(2,5));
+
+let message:string="hello,typescript";
+let fullName:string="Prakhar";
+let lastname:string="Mishra";
+let myName:string= fullName +" "+ lastname;
+console.log(myName);
+let computedValue:number = Math.sqrt(23);
+
+let mail:string="please mail me if you want to send me";
+console.log(mail);
+let maillength =mail.length;
+console.log(maillength);
+console.log(message.length);
+console.log(fullName.length);
+console.log(lastname.length);
+console.log(myName.length);
+console.log(computedValue);
---
+++ src/ts.md
@@ -0,0 +1,4 @@
+//lecture notes are hear//
+
+Advantages
+1.It helps typescript complier to enforce to type checking during compile time and provid type saftey during devlepment;
